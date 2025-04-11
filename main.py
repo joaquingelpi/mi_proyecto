bf313c164ff7838b763f92f6827c278248f9dc64
¨joaquingelpi¨ <joaquingelpi184@gmail.com>
2025-04-11 10:42:00 -0300

    Actualizacion clase 11-04

---
+++ main.py
@@ -0,0 +1,3 @@
+from operaciones import filtrar_pares
+
+print("Resultado filtrar pares:", filtrar_pares(1,2,3,4,5,6,7,8,9))
---
+++ src/operaciones.py
@@ -0,0 +1,5 @@
+def filtrar_pares(lista):
+    pares = [i for i in lista if i % 2 == 0]
+    print(pares)
+
+
